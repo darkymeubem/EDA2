2c6966fb00c77cffc55e0da4df3e47e43ed9ea89
Felipe Pedroza <73255212+darkymeubem@users.noreply.github.com>
2026-05-12 12:34:31 -0300

    update image core

--- cpp/learning/image_manipulation/src/Image.cpp
+++ cpp/learning/image_manipulation/src/Image.cpp
@@ -378,3 +378,102 @@ Image& Image::flipY(){
     
     return *this;
 }
+
+Image& Image::overlay(const Image& source, int x, int y) {
+
+	uint8_t* srcPx;
+	uint8_t* dstPx;
+
+	for(int sy = 0;sy < source.h;++sy) {
+		if(sy + y < 0) {continue;}
+		else if(sy + y >= h) {break;}
+		for(int sx = 0;sx < source.w;++sx) {
+			if(sx + x < 0) {continue;}
+			else if(sx + x >= w) {break;}
+			srcPx = &source.data[(sx + sy * source.w) * source.channels];
+			dstPx = &data[(sx + x + (sy + y) * w) * channels];
+
+			float srcAlpha = source.channels < 4 ? 1 : srcPx[3] / 255.f;
+			float dstAlpha = channels < 4 ? 1 : dstPx[3] / 255.f;
+
+			if(srcAlpha > .99 && dstAlpha > .99) {
+				if(source.channels >= channels) {
+					memcpy(dstPx, srcPx, channels);
+				}
+				else {
+					// In case our source image is grayscale and the dest one isnt
+					memset(dstPx, srcPx[0], channels);
+				}
+			}
+			else {
+				float outAlpha = srcAlpha + dstAlpha * (1 - srcAlpha);
+				if(outAlpha < .01) {
+					memset(dstPx, 0, channels);
+				}
+				else {
+					for(int chnl = 0;chnl < channels;++chnl) {
+						dstPx[chnl] = (uint8_t)BYTE_BOUND((srcPx[chnl]/255.f * srcAlpha + dstPx[chnl]/255.f * dstAlpha * (1 - srcAlpha)) / outAlpha * 255.f);
+					}
+					if(channels > 3) {dstPx[3] = (uint8_t)BYTE_BOUND(outAlpha * 255.f);}
+				}
+			}
+			
+		}
+
+	}
+	return *this;
+}
+
+// Image& Image::overlayText(const char* txt, const Font& font, int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+// 	size_t len = strlen(txt);
+// 	SFT_Char c;
+// 	int32_t dx, dy;
+// 	uint8_t* dstPx;
+// 	uint8_t srcPx;
+// 	uint8_t color[4] = {r, g, b, a};
+
+// 	for(size_t i = 0;i < len;++i) {
+// 		if(sft_char(&font.sft, txt[i], &c) != 0) {
+// 			printf("\e[31m[ERROR] Font is missing character '%c'\e[0m\n", txt[i]);
+// 			continue;
+// 		}
+
+// 		for(uint16_t sy = 0;sy < c.height;++sy) {
+// 			dy = sy + y + c.y;
+// 			if(dy < 0) {continue;}
+// 			else if(dy >= h) {break;}
+// 			for(uint16_t sx = 0;sx < c.width;++sx) {
+// 				dx = sx + x + c.x;
+// 				if(dx < 0) {continue;}
+// 				else if(dx >= w) {break;}
+// 				dstPx = &data[(dx + dy * w) * channels];
+// 				srcPx = c.image[sx + sy * c.width];
+
+// 				if(srcPx != 0) {
+// 					float srcAlpha = (srcPx / 255.f) * (a / 255.f);
+// 					float dstAlpha = channels < 4 ? 1 : dstPx[3] / 255.f;
+// 					if(srcAlpha > .99 && dstAlpha > .99) {
+// 						memcpy(dstPx, color, channels);
+// 					}
+// 					else {
+// 						float outAlpha = srcAlpha + dstAlpha * (1 - srcAlpha);
+// 						if(outAlpha < .01) {
+// 							memset(dstPx, 0, channels);
+// 						}
+// 						else {
+// 							for(int chnl = 0;chnl < channels;++chnl) {
+// 								dstPx[chnl] = (uint8_t)BYTE_BOUND((color[chnl]/255.f * srcAlpha + dstPx[chnl]/255.f * dstAlpha * (1 - srcAlpha)) / outAlpha * 255.f);
+// 							}
+// 							if(channels > 3) {dstPx[3] = (uint8_t)BYTE_BOUND(outAlpha * 255.f);}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+
+// 		x += c.advance;
+// 		free(c.image);
+// 	}
+
+// 	return *this;
+// }
