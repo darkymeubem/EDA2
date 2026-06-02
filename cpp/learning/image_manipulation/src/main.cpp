ab9deef61039da6650745361a267a356d709ed7c
Felipe Pedrozas <20222869@sarah.br>
2026-06-02 11:39:43 -0300

    wip: trabalhando no algoritmo de edge detection (based on: sobel operator)

--- cpp/learning/image_manipulation/src/main.cpp
+++ cpp/learning/image_manipulation/src/main.cpp
@@ -1,10 +1,28 @@
 #include "Image.h"
 #include <iostream>
+
+#include <cstdlib>
+#include <cmath>
+#include <chrono>
+
 int main(int argc, char** argv) {
-	Image test1("imgs/teste.png");
 	
-	Font roboto("Roboto-Regular.ttf", 100);
-	test1.overlayText("Hello, World!", roboto, 100, 100);
-	test1.write("imgs/output_overlay_text.png");
+	Image img("imgs/test5.jpg");
+
+	//para fazer edge detection a imagem precisa estar em gray scale
+	img.grayscale_lum();
+	int img_size = img.w*img.h;
+
+	Image gray_img(img.w, img.h, 1);
+
+
+	for(uint64_t k=0; k<img_size; ++k){
+		gray_img.data[k] = img.data[img.channels*k];
+	}
+
+	gray_img.write("imgs/teste5_gray.png");
+
+	//blur
+
 	return 0;
 }
